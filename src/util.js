bc979b424c7bed1c0038555e8d233d5e88a90064
Callum Macrae <callum@macr.ae>
2015-10-07 11:53:23 +0100

    Added comments to more things

--- src/util.js
+++ src/util.js
@@ -3,6 +3,13 @@
 var util = module.exports = {};
 var fix = require('fix-ev');
 
+/**
+ * Add an event handler to an element across multiple browsers.
+ *
+ * @param {string} event Name of event to listen for.
+ * @param {HTMLElement} element Event to listen on.
+ * @param {function} cb Callback to call when event is fired on element.
+ */
 util.on = function onEvent(event, element, cb) {
 	if (element.addEventListener) {
 		element.addEventListener(event, cb);
@@ -11,12 +18,27 @@ util.on = function onEvent(event, element, cb) {
 	}
 };
 
+/**
+ * Iterates through an array.
+ *
+ * @param {Array} arr The array to iterate over.
+ * @param {function} cb Function to call every iteration.
+ * @param {*} [scope] Optional scope to call the function with.
+ */
 util.each = function arrayEach(arr, cb, scope) {
 	for (var i = 0; i < arr.length; i++) {
 		cb.call(scope, arr[i], i, arr);
 	}
 };
 
+/**
+ * A reduce / fold function.
+ *
+ * @param {Array} arr The array to iterate over.
+ * @param {function} cb Function to call every iteration.
+ * @param {*} initial The initial value of the reduce.
+ * @returns {*} The value returned by the function.
+ */
 util.reduce = function arrayReduce(arr, cb, initial) {
 	if (typeof initial === 'undefined') {
 		initial = arr.splice(0, 1)[0];
@@ -32,6 +54,14 @@ util.reduce = function arrayReduce(arr, cb, initial) {
 	return acc;
 };
 
+/**
+ * Basically Array.prototype.some(). Returns true if cb is true for any member
+ * of the array.
+ *
+ * @param {Array} arr The array to iterate over.
+ * @param {function} cb Function to call every iteration.
+ * @returns {boolean} True if cb(array[i]) is sometimes true.
+ */
 util.some = function (arr, cb) {
 	for (var i = 0; i < arr.length; i++) {
 		if (cb(arr[i])) {
@@ -42,6 +72,14 @@ util.some = function (arr, cb) {
 	return false;
 };
 
+/**
+ * Basically Array.prototype.every(). Returns true if cb is true for ALL members
+ * of the array. Complements util.some().
+ *
+ * @param {Array} arr The array to iterate over.
+ * @param {function} cb Function to call every iteration.
+ * @returns {boolean} True if cb(array[i]) is true every time.
+ */
 util.every = function (arr, cb) {
 	for (var i = 0; i < arr.length; i++) {
 		if (!cb(arr[i])) {
@@ -52,6 +90,12 @@ util.every = function (arr, cb) {
 	return true;
 };
 
+/**
+ * Tests whether a thing is an Array.
+ *
+ * @param {*} obj Something which might be an Array.
+ * @returns {boolean} True if obj is an Array.
+ */
 util.isArray = function isArray(obj) {
 	return Object.prototype.toString.call(obj) === '[object Array]';
 };
